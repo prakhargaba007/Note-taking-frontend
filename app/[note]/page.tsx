8dab72e30fa1b80331c2a97c0644210997dad526
prakhargaba007 <prakhargaba@gmail.com>
2024-06-10 23:52:32 +0530

    .

--- app/[note]/page.tsx
+++ app/[note]/page.tsx
@@ -1,5 +1,14 @@
+import React from "react";
 import { BadgeCard } from "@/components/BadgeCard";
 
-export default function card({ params }) {
-  return <BadgeCard params={params.note} />;
+interface CardProps {
+  params: {
+    note: string; // Adjust the type according to what `params.note` actually is.
+  };
 }
+
+const Card: React.FC<CardProps> = ({ params }) => {
+  return <BadgeCard params={params.note} />;
+};
+
+export default Card;
